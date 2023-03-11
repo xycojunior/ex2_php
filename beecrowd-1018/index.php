4f0be0a276e90030372767700c62250a5e075aff
Francisco Junior <104867520+xycojunior@users.noreply.github.com>
2023-03-11 10:44:14 -0300

    atualização

--- beecrowd-1018/index.php
+++ beecrowd-1018/index.php
@@ -13,19 +13,19 @@
     $n = readline();
     echo("{$n}\n");
     
-    echo("{{$n}/100} nota(s) de R$ 100,00\n");
+    echo intdiv($n,100), " nota(s) de R$ 100,00\n";
     $n %= 100;
-    echo("{{$n} /50} nota(s) de R$ 50,00\n");
+    echo intdiv($n,50), " nota(s) de R$ 50,00\n";
     $n %= 50;
-    echo("{{$n} /20} nota(s) de R$ 20,00\n");
+    echo intdiv($n,20), " nota(s) de R$ 20,00\n";
     $n %= 20;
-    echo("{{$n}/10} nota(s) de R$ 10,00\n");
+    echo intdiv($n,10), " nota(s) de R$ 10,00\n";
     $n %= 10;
-    echo("{{$n} /5} nota(s) de R$ 5,00\n");
+    echo intdiv($n,5), " nota(s) de R$ 5,00\n";
     $n %= 5;
-    echo("{{$n} /2} nota(s) de R$ 2,00\n");
+    echo intdiv($n,2), " nota(s) de R$ 2,00\n";
     $n %= 2;
-    echo("{{$n} nota(s) de R$ 1,00");
+    echo "{$n} nota(s) de R$ 1,00\n";
         
     ?>
 </body>
